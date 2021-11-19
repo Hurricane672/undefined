310753a9bf9b72b6580fa2bc5423b635f5f21552
2020211822 <381058973@qq.com>
2021-11-19 16:25:21 +0800

    onno

---
+++ lib/xssghost.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time
+import sys
+
+
+def phadsys():
+    if sys.platform.startswith('win'):
+        return "./thirdparty/phantomjs/Windows/phantomjs.exe"
+    elif sys.platform.startswith('linux'):
+        return "./thirdparty/phantomjs/Linux/phantomjs"
+    else:
+        print("unsupportable system")
+        sys.exit(0)
+
+
+def main():
+    driver = webdriver.PhantomJS(executable_path=phadsys())
+    driver.get("https://www.bilibili.com/")
+    driver.get_screenshot_as_file("C:\\Users\\HP\\Desktop\\biili.png")
+
+
+if __name__ == '__main__':
+    main()

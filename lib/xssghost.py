d519aad9d04185d40430f3441c1fc69c231cae71
2020211822 <381058973@qq.com>
2021-11-21 21:30:53 +0800

    我不能理解

--- lib/xssghost.py
+++ lib/xssghost.py
@@ -1,21 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 import time
 import sys
 
+from selenium.webdriver.common import service
+
 def phadsys():
     if sys.platform.startswith('win'):
-        return "./thirdparty/phantomjs/Windows/phantomjs.exe"
+        return "D:/experimentalClass/AndyFUN/thirdparty/chrome/chromedriver-win.exe"
     elif sys.platform.startswith('linux'):
-        return "./thirdparty/phantomjs/Linux/phantomjs"
+        return "./thirdparty/chrome/chromedriver-linux"
     else:
         print("unsupportable system")
         sys.exit(0)
 
 
 def main():
-    driver = webdriver.PhantomJS(executable_path=phadsys())
+    s=Service(phadsys())
+    driver = webdriver.Chrome(service=s)
     driver.get("https://www.bilibili.com/")
     driver.get_screenshot_as_file("C:\\Users\\HP\\Desktop\\biili.png")
+    driver.quit()
 
 
 if __name__ == '__main__':

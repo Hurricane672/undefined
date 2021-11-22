4a77c924f240e136d2e6b1dfd8382e03dde33196
2020211822 <381058973@qq.com>
2021-11-22 22:00:42 +0800

    aaaa

--- lib/xssghost.py
+++ lib/xssghost.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
@@ -5,23 +6,37 @@ import sys
 
 from selenium.webdriver.common import service
 
+
 def phadsys():
     if sys.platform.startswith('win'):
-        return "D:/experimentalClass/AndyFUN/thirdparty/chrome/chromedriver-win.exe"
+        return "./thirdparty/chrome/chromedriver-win.exe"
     elif sys.platform.startswith('linux'):
         return "./thirdparty/chrome/chromedriver-linux"
     else:
         print("unsupportable system")
         sys.exit(0)
 
+def driver_settings():
+    chrome_opt = webdriver.ChromeOptions()
+    
+    chrome_opt.add_argument('--headless')
+    chrome_opt.add_argument('--disable-gpu')
+    chrome_opt.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29"')
+    
+    
+    return chrome_opt
 
 def main():
-    s=Service(phadsys())
-    driver = webdriver.Chrome(service=s)
-    driver.get("https://www.bilibili.com/")
-    driver.get_screenshot_as_file("C:\\Users\\HP\\Desktop\\biili.png")
-    driver.quit()
+    server = Service(phadsys())
 
+    driver = webdriver.Chrome(service=server,options=driver_settings())
+    driver.implicitly_wait(10) 
+
+    driver.get('https://www.baidu.com')
+    print("\n"+driver.title)
+    
+    driver.quit()
+   
 
 if __name__ == '__main__':
     main()

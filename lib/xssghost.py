0bae2971bfea1d1316c673676974b82b5645509b
2020211822 <381058973@qq.com>
2021-11-24 17:57:20 +0800

    1

--- lib/xssghost.py
+++ lib/xssghost.py
@@ -1,6 +1,7 @@
-#coding=utf-8
+# coding=utf-8
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 import time
 import sys
 
@@ -16,27 +17,46 @@ def phadsys():
         print("unsupportable system")
         sys.exit(0)
 
+
 def driver_settings():
     chrome_opt = webdriver.ChromeOptions()
-    
-    chrome_opt.add_argument('--headless')
+
+    # chrome_opt.add_argument('--headless')
     chrome_opt.add_argument('--disable-gpu')
-    chrome_opt.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29"')
-    
-    
+    chrome_opt.add_argument(
+        '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29"')
+
     return chrome_opt
+
 
 def main():
     server = Service(phadsys())
+    target = []
+    submitbtn = []
+    payload="test"
+    Ourl = 'https://www.baidu.com'
+    driver = webdriver.Chrome(service=server, options=driver_settings())
+    driver.implicitly_wait(10)
 
-    driver = webdriver.Chrome(service=server,options=driver_settings())
-    driver.implicitly_wait(10) 
+    driver.get(Ourl)
+    inputers = driver.find_elements_by_xpath("//input")
+    for inpu in inputers[:]:
+        nowtype = inpu.get_attribute("type")
+        if nowtype == "hidden":
+            inputers.remove(inpu)
+        elif nowtype == "text" or nowtype == "password":
+            target.append(inpu)
+        elif nowtype == "submit":
+            submitbtn.append(inpu)
 
-    driver.get('https://www.baidu.com')
-    print("\n"+driver.title)
-    
+    for attck in target:
+        attck.send_keys(payload+Keys.ENTER)
+    for inpu in submitbtn:
+        print(inpu.get_attribute("type"))
+
+    time.sleep(5)
     driver.quit()
-   
+
 
 if __name__ == '__main__':
     main()

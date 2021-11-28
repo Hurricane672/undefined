35b1ee5a3476f14f88213fe8e820333d8bfc07e3
2020211822 <381058973@qq.com>
2021-11-28 23:14:30 +0800

    dvwa的登录设置难度

--- lib/xssghost.py
+++ lib/xssghost.py
@@ -2,6 +2,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 import time
 import sys
 
@@ -17,6 +19,32 @@ def phadsys():
         print("unsupportable system")
         sys.exit(0)
 
+def dvwasetsecurity(driver,security):
+    driver.get(driver.current_url[:-9]+"security.php")
+    select =Select(driver.find_element(By.XPATH,"//select[@name='security']"))
+    select.select_by_visible_text(security)
+    driver.find_element(By.XPATH,"//input[@name='seclev_submit']").click()
+
+
+def dvwalogin(driver):
+    username="admin"
+    password="password"
+    inputers = driver.find_elements(By.XPATH,"//input")
+    for inpu in inputers[:]:
+        nowtype = inpu.get_attribute("type")
+        if nowtype == "hidden":
+            pass
+        elif nowtype == "text" or nowtype == "password":
+            if inpu.get_attribute("name")=="username":
+                inpu.send_keys(username)
+            elif inpu.get_attribute("name")=="password":
+                inpu.send_keys(password+Keys.ENTER)
+                break
+
+def attkdvwa(driver):
+    dvwalogin(driver)
+    security=["Low","Medium","High","Impossible"]
+    dvwasetsecurity(driver,security[0])     
 
 def driver_settings():
     chrome_opt = webdriver.ChromeOptions()
@@ -25,7 +53,8 @@ def driver_settings():
     chrome_opt.add_argument('--disable-gpu')
     chrome_opt.add_argument(
         '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29"')
-
+    chrome_opt.add_experimental_option("excludeSwitches",["enable-automation"])
+    chrome_opt.add_experimental_option("useAutomationExtension",'False')
     return chrome_opt
 
 
@@ -34,25 +63,32 @@ def main():
     target = []
     submitbtn = []
     payload="test"
-    Ourl = 'https://www.baidu.com'
+    Ourl = 'http://10.122.232.130:81/'
     driver = webdriver.Chrome(service=server, options=driver_settings())
     driver.implicitly_wait(10)
 
     driver.get(Ourl)
-    inputers = driver.find_elements_by_xpath("//input")
-    for inpu in inputers[:]:
-        nowtype = inpu.get_attribute("type")
-        if nowtype == "hidden":
-            inputers.remove(inpu)
-        elif nowtype == "text" or nowtype == "password":
-            target.append(inpu)
-        elif nowtype == "submit":
-            submitbtn.append(inpu)
+    title=driver.title
 
-    for attck in target:
-        attck.send_keys(payload+Keys.ENTER)
-    for inpu in submitbtn:
-        print(inpu.get_attribute("type"))
+    if title.find("DVWA"):
+        attkdvwa(driver)
+    else:
+        print("不是DVWA")
+    # inputers = driver.find_elements(By.XPATH,"//input")
+    # for inpu in inputers[:]:
+    #     nowtype = inpu.get_attribute("type")
+    #     if nowtype == "hidden":
+    #         inputers.remove(inpu)
+    #     elif nowtype == "text" or nowtype == "password":
+    #         target.append(inpu)
+    #     elif nowtype == "submit":
+    #         submitbtn.append(inpu)
+
+    # for attck in target:
+    #     attck.send_keys(payload)
+    # target[-1].send_keys(Keys.ENTER)
+    # for inpu in submitbtn:
+    #     print(inpu.get_attribute("type"))
 
     time.sleep(5)
     driver.quit()
